6c542cfff6c09638abd116c0d9855c0f1f81a063
r2huang <r2huang@ucsd.edu>
2022-10-11 02:21:18 -0700

    typo

--- middleware/resetEveryDayMsg.js
+++ middleware/resetEveryDayMsg.js
@@ -5,7 +5,6 @@ const Player = require(path.resolve("./model/player.js"));
 const resetPlayerHaveSpokenToday = async () => {
     Player.updateMany({}, { haveSpokenToday: false }, (err, writeResult) => { });
 };
-git
 module.exports = new CronJob('0 0 * * *', async () => {
     await resetPlayerHaveSpokenToday();
 }, null, false, 'America/Los_Angeles');
