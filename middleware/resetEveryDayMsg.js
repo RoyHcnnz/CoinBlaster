d6f89037bc23fc7c872f253c1ac64336c7a2d5b5
r2huang <r2huang@ucsd.edu>
2022-10-11 02:12:15 -0700

    updateMany fix

--- middleware/resetEveryDayMsg.js
+++ middleware/resetEveryDayMsg.js
@@ -5,7 +5,7 @@ const Player = require(path.resolve("./model/player.js"));
 const resetPlayerHaveSpokenToday = async () => {
     Player.updateMany({}, { haveSpokenToday: false }, (err, writeResult) => { });
 };
-
+git
 module.exports = new CronJob('0 0 * * *', async () => {
     await resetPlayerHaveSpokenToday();
 }, null, false, 'America/Los_Angeles');
